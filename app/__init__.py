dc1fc3a782b9eaad4938f69602af955c7f6f9d27
esoj03 <seuemail@example.com>
2025-06-05 18:45:08 -0100

    Inicio

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, migrate
+from .routes import books, revistas, stats
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.register_blueprint(books.bp)
+    app.register_blueprint(revistas.bp)
+    app.register_blueprint(stats.bp)
+
+    return app
